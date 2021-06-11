4d6186b6843785b75788a2c01250ee09cca8591a
Chris Angelico <rosuav@gmail.com>
2021-06-11 17:13:14 +1000

    Attempt to create bidirectional transport

--- WebSocket.py
+++ WebSocket.py
@@ -43,21 +43,44 @@ async def volume(sock, path):
 		# TODO: Callback to elsewhere signalling a departing client
 		del sockets[tabid]
 
-def send_volume(tabid, vol):
+async def send_message(tabid, msg):
 	if tabid not in sockets:
 		return "Gone" # Other end has gone away. Probably not a problem in practice.
+	await sockets[tabid].send(json.dumps(msg))
+
+def send_volume(tabid, vol):
+	if not stop:
+		return "Not operating" # Probably in the middle of shutdown/cleanup
 	# What happens if the buffer fills up and we start another send?
 	# Ideally: prevent subsequent sends until the first one finishes, but remember the latest
 	# volume selection made. If that's not the same as the first volume, send another after.
-	asyncio.create_task(sockets[tabid].send(json.dumps({"cmd": "setvolume", "volume": vol})))
+	asyncio.run_coroutine_threadsafe(send_message(tabid, {"cmd": "setvolume", "volume": vol}), stop.get_loop())
 
 async def listen():
-	global stop; stop = asyncio.Future()
+	global stop; stop = asyncio.get_running_loop().create_future()
 	# TODO: Add an SSL context so Chrome allows non-localhost connections
 	async with websockets.serve(volume, "localhost", 8888):
 		print("Listening.")
 		await stop
 		print("Shutting down.") # I don't hate you!
+	stop = None
+
+def halt():
+	if not stop: return "Already stopping"
+	stop.get_loop().call_soon_threadsafe(stop.set_result, "halt() called")
+
+# Yeah don't do this
+def fiddle():
+	while True:
+		import time; time.sleep(2)
+		import random; r = random.randrange(100)
+		print("Fiddling!", r)
+		if not r: break
+		for tabid in sockets: send_volume(tabid, r / 100.0)
+	halt()
+	print("Halt requested.")
+import threading; threading.Thread(target=fiddle).start()
+# End don't do this
 
 # Non-asyncio entry-point
 def run(): asyncio.run(listen())
