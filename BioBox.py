2bc8aa07d8c6e03731dbaaffa7173a39f3f45c6d
Stephen Angelico <raaff35lightning@gmail.com>
2021-07-17 12:04:46 +1000

    BioBox: Mute channel when Websocket asks nicely

--- BioBox.py
+++ BioBox.py
@@ -67,6 +67,7 @@ class MainUI(Gtk.Window):
 		print("On", tabid, ": Volume:", volume, "Muted:", bool(mute_state))
 		channel = tabs[tabid]
 		channel.update_position(int(volume * 100)) # Truncate or round?
+		channel.mute.set_active(int(mute_state))
 
 	def add_module(self, module):
 		self.modules.pack_start(module, True, True, 0)
