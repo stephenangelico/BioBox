b7e9519c5fd76e3ebed9da7ff366b9ed0c352e21
Stephen Angelico <raaff35lightning@gmail.com>
2021-07-24 11:44:59 +1000

    Analog: Init goal_completed to 0 instead of now

--- Analog.py
+++ Analog.py
@@ -97,7 +97,7 @@ def read_value():
 	global goal
 	last_speed = None
 	last_dir = None
-	goal_completed = time.monotonic()
+	goal_completed = 0
 	for pos in read_position():
 		if goal is not None:
 			if goal < 0:
