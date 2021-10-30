a33e3e7f793ebdf1deb48e8aebaf0492139f1565
Stephen Angelico <raaff35lightning@gmail.com>
2021-10-30 12:08:49 +1100

    BioBox: Add CSS framework to window

--- BioBox.py
+++ BioBox.py
@@ -11,7 +11,7 @@ import json
 
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk, Gdk, GLib
 
 try:
 	import Analog
@@ -372,6 +372,14 @@ class Browser(Channel):
 		WebSocket.set_muted(self.tabid, mute_state)
 
 if __name__ == "__main__":
+	css = b"window {-gtk-dpi: 90;}"
+	style_provider = Gtk.CssProvider()
+	style_provider.load_from_data(css)
+	Gtk.StyleContext.add_provider_for_screen(
+		Gdk.Screen.get_default(),
+		style_provider,
+		Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+	)
 	win = MainUI()
 	win.connect("destroy", Gtk.main_quit)
 	win.show_all()
