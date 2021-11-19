80d5cfd04563c5a7599b40695ab47a29214c5238
Chris Angelico <rosuav@gmail.com>
2021-11-20 10:59:35 +1100

    Make WebSocket into a task rather than a thread

--- WebSocket.py
+++ WebSocket.py
@@ -11,7 +11,6 @@ from pprint import pprint
 import websockets # ImportError? pip install websockets
 
 sockets = { }
-stop = None # If this exists, it's a Future that can be set to halt the event loop
 callbacks = { }
 
 async def volume(sock, path):
@@ -50,22 +49,17 @@ async def send_message(tabid, msg):
 		return "Gone" # Other end has gone away. Probably not a problem in practice.
 	await sockets[tabid].send(json.dumps(msg))
 
-def set_volume(tabid, vol):
-	if not stop:
-		return "Not operating" # Probably in the middle of shutdown/cleanup
+async def set_volume(tabid, vol):
 	# What happens if the buffer fills up and we start another send?
 	# Ideally: prevent subsequent sends until the first one finishes, but remember the latest
 	# volume selection made. If that's not the same as the first volume, send another after.
-	asyncio.run_coroutine_threadsafe(send_message(tabid, {"cmd": "setvolume", "volume": vol}), stop.get_loop())
+	await send_message(tabid, {"cmd": "setvolume", "volume": vol})
 
-def set_muted(tabid, muted):
-	if not stop:
-		return "Not operating"
-	asyncio.run_coroutine_threadsafe(send_message(tabid, {"cmd": "setmuted", "muted": bool(muted)}), stop.get_loop())
+async def set_muted(tabid, muted):
+	await send_message(tabid, {"cmd": "setmuted", "muted": bool(muted)})
 
-async def listen(*, connected=None, disconnected=None, volumechanged=None, host="", port=8888):
+async def listen(*, stop, connected=None, disconnected=None, volumechanged=None, host="", port=8888):
 	callbacks.update(connected=connected, disconnected=disconnected, volumechanged=volumechanged)
-	global stop; stop = asyncio.get_running_loop().create_future()
 	ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 	try:
 		ssl_context.load_cert_chain("fullchain.pem", "privkey.pem")
@@ -74,29 +68,11 @@ async def listen(*, connected=None, disconnected=None, volumechanged=None, host=
 		ssl_context = None
 	async with websockets.serve(volume, host, port, ssl=ssl_context):
 		print("Listening.")
-		await stop
+		await stop.wait()
 		print("Shutting down.") # I don't hate you!
-	stop = None
-
-def halt():
-	if not stop: return "Already stopping"
-	stop.get_loop().call_soon_threadsafe(stop.set_result, "halt() called")
-
-# Yeah don't do this
-def fiddle():
-	while True:
-		import time; time.sleep(2)
-		import random; r = random.randrange(100)
-		print("Fiddling!", r)
-		if not r: break
-		for tabid in sockets: set_volume(tabid, r / 100.0)
-	halt()
-	print("Halt requested.")
-#import threading; threading.Thread(target=fiddle).start()
-# End don't do this
 
 # Non-asyncio entry-point
-def run(**kw): asyncio.run(listen(**kw))
+def run(**kw): asyncio.run(listen(stop=asyncio.Event(), **kw))
 if __name__ == "__main__":
 	try:
 		run()
