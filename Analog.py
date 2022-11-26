ebcefb7b9da4a58f1177c5d9a7360d4c84794df6
Stephen Angelico <raaff35lightning@gmail.com>
2022-11-26 12:45:39 +1100

    Analog: Change order of values to print

--- Analog.py
+++ Analog.py
@@ -104,7 +104,7 @@ async def read_value():
 					dir = Motor.brake
 					goal = None
 					goal_completed = time.monotonic()
-				print(dir.__name__, speed, goal, dist, braked * "Brakes engaged", end="\x1b[K\r")
+				print(goal, dist, speed, dir.__name__, braked * "Brakes engaged")
 				if speed != last_speed:
 					Motor.speed(speed)
 					last_speed = speed
