490e27441bf6a706ea16f944be57f559f431afd5
Stephen Angelico <raaff35lightning@gmail.com>
2022-12-24 12:09:00 +1100

    ease: Replace static dist->speed mapping with dynamically floored calculation

--- ease.py
+++ ease.py
@@ -23,15 +23,17 @@ except RuntimeError:
 goal = None
 goal_set = 0
 current_tick = 0
+min_speed = 24
 ACCEL_LIMIT = 10
 
 def randomly_change_goal():
-	global goal, goal_set
+	global goal, goal_set, min_speed
 	new_goal = random.randrange(1024 * 64 * 4) # On average, change the goal every 1 second
 	if new_goal < 1024:
 		print("SETTING A NEW GOAL:", new_goal)
 		goal_set = current_tick
 		goal = new_goal
+		min_speed = 24
 
 def remap_range(raw): # could be imported from Analog if on the Pi
 	return max(0, min(1023 - raw, 1023))
@@ -47,10 +49,12 @@ async def read_position():
 		yield remap_range(pot)
 
 async def move_slider():
-	global goal, current_tick
+	global goal, current_tick, min_speed
 	last_speed = 0 # Assume we start out not moving
+	last_desired_speed = 0
 	last_pos = 0
 	overshoot = 0
+	stopped = False # Flag for waiting one tick to see if we're stopped at the goal
 	async for pos in read_position():
 		current_tick += 1
 		randomly_change_goal() # Simulate external signals that change the goal
@@ -59,26 +63,25 @@ async def move_slider():
 		dist = abs(pos - goal)
 		if dist < 1:
 			speed = 0
-		elif dist < 4:
-			speed = 10
-		elif dist < 16:
-			speed = 20
-		elif dist < 64:
-			speed = 40
-		elif dist < 256:
-			speed = 60
 		elif dist < 512:
-			speed = 80
+			speed = int(dist ** 0.5 * 4)
+			if speed < min_speed:
+				speed = min_speed
 		else:
 			speed = 100
 		if goal < pos: speed = -speed
 
-		if (speed > 0 and last_speed < 0) or (speed < 0 and last_speed > 0):
+		if (speed > 0 and last_desired_speed < 0) or (speed < 0 and last_desired_speed > 0):
+			print("Overshoot!")
+			min_speed = min_speed // 2
 			if overshoot < goal_set:
 				overshoot = current_tick
+		last_desired_speed = speed
 		accel = speed - last_speed # Desired acceleration
 		accel = max(-ACCEL_LIMIT, min(accel, ACCEL_LIMIT)) # Actual acceleration
-		last_speed = speed = last_speed + accel # Actual speed
+		if dist >= 1:
+			speed = last_speed + accel # Actual speed
+		last_speed = speed
 		# Note that trying to limit the rate of change of acceleration (the jerk
 		# force) is hard to do statelessly without ever exceeding the desired
 		# speed. The only way to do it is to begin reducing the acceleration rate
@@ -95,12 +98,15 @@ async def move_slider():
 			# Note that we might temporarily pause during a direction change;
 			# this doesn't count as reaching the goal.
 			if dist < 1:
-				goal = None
-				print("GOAL REACHED in", current_tick - goal_set)
-				if overshoot >= goal_set:
-					print("Overshoot", current_tick - overshoot)
-					# Note that spurious overshoots can be reported if a goal is overwritten.
-				continue
+				if stopped:
+					goal = None
+					print("GOAL REACHED in", current_tick - goal_set)
+					if overshoot >= goal_set:
+						print("Overshoot", current_tick - overshoot)
+						# Note that spurious overshoots can be reported if a goal is overwritten.
+					continue
+				else:
+					stopped = True
 		Motor.speed(abs(speed)) # Is it okay to set speed to zero? If not, move this into both the conditions above.
 		print(f"[{time.time():12f}] {pos=:4d} {speed=:4d} {accel=:3d} {dist=:4d} dpos={pos-last_pos}")
 		last_pos = pos
