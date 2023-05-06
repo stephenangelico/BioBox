c3c087b597e0ff9a4cd9f16b18d241c31ed4ad1f
Stephen Angelico <raaff35lightning@gmail.com>
2023-05-06 12:52:06 +1000

    Break out VLC module into separate file
    Side note: this was surprisingly painless. Would be a pity if something broke
    when it gets to the Pi...

--- BioBox.py
+++ BioBox.py
@@ -1,7 +1,5 @@
-import os
 import time
 import json
-import subprocess
 import builtins
 import traceback
 import asyncio
@@ -128,38 +126,6 @@ def init_motor_pos():
 		Analog.goal = selected_channel.slider.get_value() / scale_max * 1023
 	else:
 		Analog.goal = 1023
-
-# VLC
-async def vlc():
-	vlc_module = None
-	try:
-		reader, writer = await asyncio.open_connection(config.host, config.vlc_port)
-		writer.write(b"volume\r\nmuted\r\n") # Ask volume and mute state
-		await writer.drain()
-		vlc_module = VLC(writer)
-		await vlc_buf_read(vlc_module, reader)
-	except ConnectionRefusedError:
-		print("Could not connect to VLC on %s:%s - is TMV running?" % (config.host, config.vlc_port))
-	finally:
-		if vlc_module:
-			vlc_module.remove()
-			writer.close() # Close connection and remove module
-			await writer.wait_closed()
-		print("VLC cleanup done")
-
-async def vlc_buf_read(vlc_module, reader):
-	while True:
-		data = await reader.readline()
-		if not data:
-			break
-		line = data.decode("utf-8")
-		attr, value = line.split(":", 1)
-		if attr == "volume":
-			vlc_module.refract_value(float(value), "backend")
-		elif attr == "muted":
-			vlc_module.mute.set_active(int(value))
-		else:
-			print("From VLC:", attr, value)
 
 # Browser
 def new_tab(tabid, host):
@@ -306,26 +272,7 @@ class Channel(Gtk.Frame):
 		print("Removing:", self.channel_name)
 		self.group.remove(self)
 
-# TODO: Break out into separate file
-class VLC(Channel):
-	group_name = "VLC"
-	step = 1.0
-
-	def __init__(self, writer):
-		super().__init__(name="VLC")
-		self.writer = writer
-
-	def write_external(self, value):
-		self.writer.write(b"volume %d \r\n" %value)
-		spawn(self.writer.drain())
-		print("To VLC: ", value)
-
-	def muted(self, widget):
-		mute_state = super().muted(widget)
-		self.writer.write(b"muted %d \r\n" %mute_state)
-		spawn(self.writer.drain())
-		print("VLC Mute status:", mute_state)
-
+import vlc
 import webcam
 import obs
 
@@ -368,7 +315,7 @@ async def main():
 	class Task():
 		running = {}
 		def VLC():
-			return vlc()
+			return vlc.vlc()
 		def Webcam():
 			return webcam.webcam()
 		def OBSModule():
---
+++ vlc.py
@@ -0,0 +1,54 @@
+import asyncio
+import config # ImportError? See config_example.py
+
+
+async def vlc():
+	vlc_module = None
+	try:
+		reader, writer = await asyncio.open_connection(config.host, config.vlc_port)
+		writer.write(b"volume\r\nmuted\r\n") # Ask volume and mute state
+		await writer.drain()
+		vlc_module = VLC(writer)
+		await vlc_buf_read(vlc_module, reader)
+	except ConnectionRefusedError:
+		print("Could not connect to VLC on %s:%s - is TMV running?" % (config.host, config.vlc_port))
+	finally:
+		if vlc_module:
+			vlc_module.remove()
+			writer.close() # Close connection and remove module
+			await writer.wait_closed()
+		print("VLC cleanup done")
+
+async def vlc_buf_read(vlc_module, reader):
+	while True:
+		data = await reader.readline()
+		if not data:
+			break
+		line = data.decode("utf-8")
+		attr, value = line.split(":", 1)
+		if attr == "volume":
+			vlc_module.refract_value(float(value), "backend")
+		elif attr == "muted":
+			vlc_module.mute.set_active(int(value))
+		else:
+			print("From VLC:", attr, value)
+
+
+class VLC(Channel):
+	group_name = "VLC"
+	step = 1.0
+
+	def __init__(self, writer):
+		super().__init__(name="VLC")
+		self.writer = writer
+
+	def write_external(self, value):
+		self.writer.write(b"volume %d \r\n" %value)
+		spawn(self.writer.drain())
+		print("To VLC: ", value)
+
+	def muted(self, widget):
+		mute_state = super().muted(widget)
+		self.writer.write(b"muted %d \r\n" %mute_state)
+		spawn(self.writer.drain())
+		print("VLC Mute status:", mute_state)
