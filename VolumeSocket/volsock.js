cb3cd857fe8974d075c60a6d2aa0636934287253
Stephen Angelico <raaff35lightning@gmail.com>
2024-04-13 12:14:05 +1000

    volsock: Get YT player object differently for www/yt and music/yt

--- VolumeSocket/volsock.js
+++ VolumeSocket/volsock.js
@@ -2,21 +2,28 @@
 //How does that signal that a page no longer meets the criterion?
 //NOTE: This broadly assumes only one important video object, which is
 //always present. It might work with multiple but isn't guaranteed.
-let extID = "oejgabkmelnodicghenecnopnnninpmm"
+let extID = "oejgabkmelnodicghenecnopnnninpmm";
 // Content scripts runnning in the MAIN world basically become page scripts, and
 // therefore lose their extension ID. Because it is extremely difficult for a
 // page script to communicate with an extension without knowing the ID for that
 // extension, it is easier to simply embed that ID here for all communications.
 // This ID is current for me, on my systems, with an unpacked extension. For
 // distribution, a new and stable extension ID will be generated.
+let player;
 
 function init(extID)
 {
 	let port = chrome.runtime.connect(extID);
-	port.postMessage({cmd: "newtab", host: location.host})
-	port.onMessage.addListener(extListen)
+	port.postMessage({cmd: "newtab", host: location.host});
+	port.onMessage.addListener(extListen);
 	if (location.host === "www.youtube.com" || location.host === "music.youtube.com") {
-		const player = document.getElementById('movie_player');
+		if (location.host === "www.youtube.com") {
+				player = document.getElementById('movie_player');
+		}
+		if (location.host === "music.youtube.com") {
+				player = document.getElementById('player-page').playerUiService.playerApi;
+				// So far the UI doesn't react at all
+		}
 		document.querySelectorAll("video").forEach(vid =>
 		(vid.onvolumechange = e => port.postMessage({cmd: "volumechanged", volume: player.getVolume() / 100, muted: player.isMuted()}))()
 		);
@@ -30,7 +37,6 @@ function extListen(message)
 	switch (message.cmd) {
 		case "volume":
 			if (location.host === "www.youtube.com" || location.host === "music.youtube.com") {
-				const player = document.getElementById('movie_player');
 				player.setVolume(message.value * 100);
 				sessionStorage.setItem("yt-player-volume", JSON.stringify({
 					creation: +new Date, expiration: +new Date + 2592000000, data: JSON.stringify({volume: player.getVolume(), muted: player.isMuted()})
