800ff1d28c8fab6aff1172c52d685506e491317d
Stephen Angelico <raaff35lightning@gmail.com>
2024-06-01 09:50:27 +1000

    background: Remove a TODO that was done weeks ago

--- VolumeSocket/background.js
+++ VolumeSocket/background.js
@@ -1,5 +1,4 @@
 // Service Worker for VolSock
-// TODO: Migrate communication with BioBox here from volsock.js
 // volsock.js will handle interaction with the page ie reading and writing to
 // the slider in whatever way is appropriate for the site, while the service
 // worker will handle the connection to BioBox. This will satisfy the Content
